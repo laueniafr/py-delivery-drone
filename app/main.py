a16f30b2153e142ccbe4ea8b61afef66c59819ac
Lauênia Ferreira <laueniafr@outlook.com>
2025-05-05 09:05:14 -0300

    fixing line is too long

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: Optional[List[int]] = None) -> None:
+    def __init__(self, name: str,
+                 weight: int,
+                 coords: Optional[List[int]] = None) -> None:
         self.name: str = name
         self.weight: int = weight
         self.coords: List[int] = coords.copy() if coords else [0, 0]
@@ -29,7 +31,9 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: Optional[List[int]] = None) -> None:
+    def __init__(self, name: str,
+                 weight: int,
+                 coords: Optional[List[int]] = None) -> None:
         coords = coords if coords else [0, 0, 0]
         super().__init__(name, weight, coords)
 
